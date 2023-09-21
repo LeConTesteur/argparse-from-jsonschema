8e67b6632520abe603ef2eef703708d58c8cbfc6
Le ConTesteur <>
2023-09-21 20:44:03 +0200

    Append keywords information into setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from setuptools import setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+KEYWORDS = ('Argparse,jsonschema,Argument Schema,'
+            'data validation, json, validation,'
+            'command, line, parser, parsing')
+
 name = 'argparse-from-jsonschema'
 module = name.replace("-", "_")
 setup(
     name=name,
-    version='0.0.5',
+    version='0.0.6',
     description='Parse Argument with JSON Schema',
     url=f'https://github.com/LeConTesteur/{name}',
     author='LeConTesteur',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    keywords='Argument Schema',
+    keywords=KEYWORDS,
     entry_points={
         'console_scripts': [f'{name}={module}:main'],
     },
